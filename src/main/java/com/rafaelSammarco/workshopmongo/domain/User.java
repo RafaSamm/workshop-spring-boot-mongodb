9048bf258968ea7b431e402e72ecef9f276d99ad
RafaSamm <rafarhs286@gmail.com>
2023-05-22 11:38:26 -0300

    Operação de instanciação da base de dados

--- src/main/java/com/rafaelSammarco/workshopmongo/domain/User.java
+++ src/main/java/com/rafaelSammarco/workshopmongo/domain/User.java
@@ -3,11 +3,14 @@ package com.rafaelSammarco.workshopmongo.domain;
 import jakarta.persistence.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
 
 @Document // (collection = "user")
 public class User implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     @Id
     private String id;
     private String name;
