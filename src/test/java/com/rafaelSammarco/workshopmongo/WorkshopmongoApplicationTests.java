ee88f5a999172b14a387693ae5eaee008e3627ea
RafaSamm <rafarhs286@gmail.com>
2023-05-22 08:46:35 -0300

    Primeiro commit - Projeto criado

---
+++ src/test/java/com/rafaelSammarco/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafaelSammarco.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
